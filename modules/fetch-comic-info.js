dfb807d9d33fffb1b4640aae5fecd699a21cf7d5
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2022-08-21 11:40:24 +0100

    Changed function name and added error message if comic info fetch fails

--- modules/fetch-comic-info.js
+++ modules/fetch-comic-info.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const axios = require("axios");
 const cryptoJs = require("crypto-js");
 const { getDateMetadata, getCreators } = require("./helper-functions");
-const scrapePage = require("./scrape-comic-page");
+const addScrapedComicInfo = require("./scrape-comic-page");
 
 // Base URL
 const marvelApiUrl = "https://gateway.marvel.com:443/v1/public/";
@@ -37,43 +37,48 @@ async function getComicInfo(comicId) {
 // Create the comic object used to populate the XML file
 async function createComicObject(comicId) {
     const comicInfo = await getComicInfo(comicId);
-    const comicInfoUrl = comicInfo.urls[0].url.split("?");
-    const comicDate = getDateMetadata(comicInfo.dates);
-    const creators = getCreators(comicInfo.creators);
 
-    const comicObj = {
-        id: comicInfo.id,
-        title: comicInfo.title,
-        summary: comicInfo.description || "",
-        number: comicInfo.issueNumber,
-        web: comicInfoUrl[0],
-        series: comicInfo.series.name,
-        volume: comicDate.year,
-        ageRating: "",
-        publisher: "Marvel",
-        genre: "Comics",
-        languageISO: "en",
-        count: comicInfo.pageCount,
-        seriesGroup: comicInfo.series.name,
-        year: comicDate.year,
-        month: comicDate.month,
-        day: comicDate.day,
-        writer: creators.writer,
-        penciler: creators.penciler,
-        inker: creators.inker,
-        colorist: creators.colorist,
-        letterer: creators.letterer,
-        coverArtist: "",
-        editor: creators.editor,
-    };
+    if (comicInfo) {
+        const comicInfoUrl = comicInfo.urls[0].url.split("?");
+        const comicDate = getDateMetadata(comicInfo.dates);
+        const creators = getCreators(comicInfo.creators);
 
-    if (!comicObj.summary || comicObj.coverArtist) {
-        const scrapedComicInfo = await scrapePage(comicObj)
-        comicObj.summary = scrapedComicInfo.summary;
-        comicObj.coverArtist = scrapedComicInfo.coverArtist;
+        const comicObj = {
+            id: comicInfo.id,
+            title: comicInfo.title,
+            summary: comicInfo.description || "",
+            number: comicInfo.issueNumber,
+            web: comicInfoUrl[0],
+            series: comicInfo.series.name,
+            volume: comicDate.year,
+            ageRating: "",
+            publisher: "Marvel",
+            genre: "Comics",
+            languageISO: "en",
+            count: comicInfo.pageCount,
+            seriesGroup: comicInfo.series.name,
+            year: comicDate.year,
+            month: comicDate.month,
+            day: comicDate.day,
+            writer: creators.writer,
+            penciler: creators.penciler,
+            inker: creators.inker,
+            colorist: creators.colorist,
+            letterer: creators.letterer,
+            coverArtist: "",
+            editor: creators.editor,
+        };
+
+        if (!comicObj.summary || comicObj.coverArtist) {
+            const scrapedComicInfo = await addScrapedComicInfo(comicObj);
+            comicObj.summary = scrapedComicInfo.summary;
+            comicObj.coverArtist = scrapedComicInfo.coverArtist;
+        }
+
+        return comicObj;
     }
 
-    return comicObj;
+    console.log(`Could not get comic info for ${comicId}. Please try again later`);
 }
 
 module.exports = createComicObject;
