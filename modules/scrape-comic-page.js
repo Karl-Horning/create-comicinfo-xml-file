8f0eb0f304d270a87abca39ae0033f0fd0463f1b
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2022-08-21 10:16:53 +0100

    Updated comments for clarity

--- modules/scrape-comic-page.js
+++ modules/scrape-comic-page.js
@@ -3,39 +3,33 @@ const puppeteer = require("puppeteer");
 
 // 95737
 async function scrapePage(comicObj) {
+    // Use the URL to scrape the comic page
     const url = comicObj.web;
     const scrapedComicInfo = {
         summary: "",
         coverArtist: "",
     };
 
-    // Use Puppeteer to get HTML
+    // Use Puppeteer to get the HTML
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     const html = await page.content();
     await browser.close();
 
-    // Load HTML in Cheerio
+    // Load the HTML in Cheerio
     const $ = cheerio.load(html);
 
-    // User Cheerio to sift through HTML for summary
+    // User Cheerio to sift through the HTML for the summary
     const summarySection = $(".featured-item-desc");
     const doubleSummaryText = summarySection.text().toString().trim();
     const summaryTextArr = doubleSummaryText.split("\n");
-    
+
     scrapedComicInfo.summary = summaryTextArr[0].trim();
 
     return scrapedComicInfo;
 }
 
-// Use the URL to scrape the comic page
-// comicObj.web
-// http://marvel.com/comics/issue/95738/the_amazing_spider-man_2022_2
-
-// Get the summary using 'data-blurb' and the comic ID:
-// document.querySelector(`[data-blurb="${comicObj.id}"]`).innerText
-// document.querySelector('[data-blurb="957383"]').innerText
 
 // Get the Cover Artist:
 // Get the length of the creator list
