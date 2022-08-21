0ae65028ac0cd8c354ce0a4909460c7a11ef16db
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2022-08-21 11:35:34 +0100

    Split into functions and added basic error handling

--- modules/scrape-comic-page.js
+++ modules/scrape-comic-page.js
@@ -1,38 +1,64 @@
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
-async function scrapePage(comicObj) {
+async function scrapePage(url) {
+    try {
+        // Use Puppeteer to get the HTML
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        const html = await page.content();
+        await browser.close();
+        return html;
+    } catch (error) {
+        console.warn(error);
+    }
+}
+
+function getComicSummary(html) {
+    // Load the HTML in Cheerio
+    const $ = cheerio.load(html);
+
+    // User Cheerio to sift through the HTML for the summary and return it
+    const summarySection = $(".featured-item-desc");
+    // The summary section is repeated, so return only the first section
+    const doubleSummaryText = summarySection.text().toString().trim();
+    const summaryTextArr = doubleSummaryText.split("\n");
+    const summaryText = summaryTextArr[0].trim();
+
+    return summaryText;
+}
+
+function getCoverArtist(html) {
+    // Load the HTML in Cheerio
+    const $ = cheerio.load(html);
+
+    // Use the classes and children tags to get cover artist
+    const creatorList = $(".creatorList > li:last > div:last > a").html();
+    // Replace the whitespace with a single space
+    const coverArtist = creatorList.replaceAll(/\s+/g, " ");
+
+    return coverArtist;
+}
+
+async function addScrapedComicInfo(comicObj) {
     // Use the URL to scrape the comic page
     const url = comicObj.web;
+    // Return object
     const scrapedComicInfo = {
         summary: "",
         coverArtist: "",
     };
 
-    // Use Puppeteer to get the HTML
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const html = await page.content();
-    await browser.close();
+    // Scrape the webpage
+    const html = await scrapePage(url);
 
-    // Load the HTML in Cheerio
-    const $ = cheerio.load(html);
-
-    // User Cheerio to sift through the HTML for the summary
-    const summarySection = $(".featured-item-desc");
-    const doubleSummaryText = summarySection.text().toString().trim();
-    const summaryTextArr = doubleSummaryText.split("\n");
-
-    // Use the classes and children tags to get cover artist
-    const creatorList = $(".creatorList > li:last > div:last a").html();
-    // Replace the whitespace with a single space
-    const coverArtist = creatorList.replaceAll(/\s+/g, " ");
-    // Assign the scraped info to the return object
-    scrapedComicInfo.coverArtist = coverArtist;
-    scrapedComicInfo.summary = summaryTextArr[0].trim();
+    if (html) {
+        scrapedComicInfo.coverArtist = getCoverArtist(html);
+        scrapedComicInfo.summary = getComicSummary(html);
+    }
 
     return scrapedComicInfo;
 }
 
-module.exports = scrapePage;
+module.exports = addScrapedComicInfo;
