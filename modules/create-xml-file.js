08fcb3ebb36132e34696c49624e29772d1808912
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2022-08-21 11:58:19 +0100

    File & function to create XML file added

---
+++ modules/create-xml-file.js
@@ -0,0 +1,15 @@
+const createFileText = require("./create-file-text");
+const fs = require("fs");
+const fileName = "./output/ComicInfo.xml";
+
+async function createXmlFile(comicId) {
+    const fileText = await createFileText(comicId);
+    const successMessage = `The XML file for ${comicId} has been created successfully!`;
+
+    fs.writeFile(fileName, fileText, (err) => {
+        if (err) throw err;
+        console.log(successMessage);
+    });
+}
+
+module.exports = createXmlFile;
