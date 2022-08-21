7764aa003801d06310bf1e8972bf18380a6d9322
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2022-08-21 14:38:34 +0100

    Moved output location to downloads folder

--- modules/create-xml-file.js
+++ modules/create-xml-file.js
@@ -1,17 +1,19 @@
 const createFileText = require("./create-file-text");
 const fs = require("fs");
-const fileName = "./output/ComicInfo.xml";
+const os = require("os");
+const userHomeDir = os.homedir();
+const fileName = `${userHomeDir}/Downloads/ComicInfo.xml`;
 
 async function createXmlFile(comicId) {
     const fileText = await createFileText(comicId);
 
     if (fileText) {
         const successMessage = `The XML file for ${comicId} has been created successfully!`;
-    
+
         fs.writeFile(fileName, fileText, (err) => {
             if (err) throw err;
             console.log(successMessage);
-        });    
+        });
     }
 }
 
