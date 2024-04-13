c84eee66f9758a8e0af8a917229aeb03aaef2f68
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-13 20:02:40 +0100

    Add JSDoc, check if API keys are available, and check if comic ID is valid

--- modules/fetch-comic-info.js
+++ modules/fetch-comic-info.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
+
 // Modules
 const axios = require("axios");
 const cryptoJs = require("crypto-js");
 const { getDateMetadata, getCreators } = require("./helper-functions");
+const { isValidComicId } = require("../src/helpers");
 const addScrapedComicInfo = require("./scrape-comic-page");
 
 // Base URL
@@ -13,8 +15,24 @@ const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 
-// Get comic info from Marvel API
-async function getComicInfo(comicId) {
+// Check if required environment variables are present and not empty
+if (
+    !process.env.API_KEY ||
+    !process.env.PRIVATE_KEY ||
+    !process.env.PUBLIC_KEY
+) {
+    console.error(
+        "Missing required environment variables. Please make sure API_KEY, PRIVATE_KEY, and PUBLIC_KEY are set."
+    );
+    process.exit(1); // Exit the process with a non-zero status code to indicate failure
+}
+
+/**
+ * Retrieves comic information from the Marvel API.
+ * @param {number} comicId - The ID of the comic to retrieve information for.
+ * @returns {Promise<object>} A promise that resolves with the comic information.
+ */
+const getComicInfo = async (comicId) => {
     // API GET requirements
     const ts = Number(new Date());
     const hash = cryptoJs.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
@@ -30,12 +48,23 @@ async function getComicInfo(comicId) {
             console.warn(`No results for ${comicId}`);
         }
     } catch (error) {
-        console.error(`${error.response.data.code}: ${error.response.data.status} (${comicId})`);
+        console.error(
+            `${error.response.data.code}: ${error.response.data.status} (${comicId})`
+        );
     }
-}
+};
 
-// Create the comic object used to populate the XML file
-async function createComicObject(comicId) {
+/**
+ * Creates a comic object used to populate the XML file.
+ * @param {number} comicId - The ID of the comic to create the object for.
+ * @returns {Promise<object>} A promise that resolves with the created comic object.
+ */
+const createComicObject = async (comicId) => {
+    if (!isValidComicId(comicId)) {
+        console.error("Incorrect comic ID. Please make sure comic ID is set.");
+        process.exit(1);
+    }
+
     const comicInfo = await getComicInfo(comicId);
 
     if (comicInfo) {
@@ -77,6 +106,6 @@ async function createComicObject(comicId) {
 
         return comicObj;
     }
-}
+};
 
 module.exports = createComicObject;
