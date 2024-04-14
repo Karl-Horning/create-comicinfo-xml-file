0c88b9c798f1f9f42eee933842a355016dd9304d
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-04-14 17:46:45 +0100

    Update init with JSDoc and error handling

--- index.js
+++ index.js
@@ -3,34 +3,40 @@ const { getScrapedComicInfo } = require("./src/modules/scrapeComicData.module");
 const { createComicObject } = require("./src/modules/createComicObject.module");
 const { createXmlFile } = require("./src/modules/createComicXmlFile.module");
 
+/**
+ * Initializes the process of fetching comic data, scraping additional info, creating a comic object, and generating an XML file.
+ * @param {string} comicId - The ID of the comic to initialize the process for.
+ */
 const init = async (comicId) => {
-    // Get the comic from the API by its ID
-    const comicApiInfo = await getComicApiInfo(comicId);
+    try {
+        // Get the comic from the API by its ID
+        console.log(`Getting comic data for ${comicId}`);
+        const comicApiInfo = await getComicApiInfo(comicId);
 
-    if (comicApiInfo) {
-        // Log the comic info for testing
-        console.log("comicApiInfo:", comicApiInfo);
+        if (!comicApiInfo) {
+            console.error(`No comic data found for ${comicId}`);
+            return;
+        }
 
         // Get the URL of the page for the comic (where the additional info can be scraped from)
+        console.log(`Scraping comic data for ${comicId}`);
         const comicInfoUrl = comicApiInfo.urls[0].url.split("?")[0];
         const scrapedComicInfo = await getScrapedComicInfo(comicInfoUrl);
 
-        // Log the scraped comic info for testing
-        console.log("scrapedComicInfo:", scrapedComicInfo);
-
         // Create the comic object with the API data, scraped data, and URL of the page for the comic
+        console.log(`Creating comic object for ${comicId}`);
         const comicObject = createComicObject(
             comicApiInfo,
             scrapedComicInfo,
             comicInfoUrl
         );
 
-        // Log the created comic object for testing
-        console.log("comicObject:", comicObject);
-
         // Create the XML file
+        console.log(`Creating the XML file for ${comicId}`);
         createXmlFile(comicObject);
+    } catch (error) {
+        console.error("An error occurred:", error);
     }
 };
 
-init("98721");
+init("101079");
